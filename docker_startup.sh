1403ff9c826d9f0575471634e789edf529955a08
M. Sonntag <michael.p.sonntag@gmail.com>
2021-12-13 14:21:23 +0100

    [Dockerfile] Set log4j security safeguard env var

--- docker_startup.sh
+++ docker_startup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
-# Mitigation of log4j vulnerability; required until the built in
-# log4j version in fuseki-server.jar has been updated to >= 2.15.0
-# Check fuseki-server.jar:METAINF/DEPENDENCIES for the used version.
-# Check https://logging.apache.org/log4j/2.x/security.html for details
-LOG4J_FORMAT_MSG_NO_LOOKUPS=true
-
 # Cleanup leftover database lock file if a server crash has happened,
 # otherwise the service can encounter restart issues.
 find /content/system/ -type f -name "tdb.lock" -delete
